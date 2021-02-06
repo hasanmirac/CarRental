ad943c2e39dc9f062f39aad7cd7ef16e3390ef27
hasanmirac <61428133+hasanmirac@users.noreply.github.com>
2021-02-06 18:28:31 +0300

    Add project file

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -21,5 +21,15 @@ namespace Business.Concrete
             // İş kodları
             return _carDal.GetAll();
         }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
     }
 }
